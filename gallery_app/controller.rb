8bbeaceafff2eeabde112ba283fb68f4c20487ae
marcinsukacz <marcin.sukacz@gmail.com>
2019-06-02 18:00:25 +0100

    view for management and drop down menu

--- gallery_app/controller.rb
+++ gallery_app/controller.rb
@@ -8,3 +8,18 @@ get '/visitors' do
   @exibitions = Exibition.all
   erb(:visitor_view)
 end
+
+get '/management/exibitions' do
+  @exibitions = Exibition.all
+  erb(:manager_view)
+end
+
+get 'management/artists' do
+  @artists = Artist.all
+  erb(:artists)
+end
+
+get '/management/exibitions/:id' do
+  @exibition = Exibition.find(params['id'])
+  erb(:show_exibition)
+end
